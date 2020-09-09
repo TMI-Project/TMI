9af30eba3f15eee924221455558790f49fed6c26
sunrabbit123 <qudwls185@naver.com>
2020-09-09 17:09:43 +0900

    Fix check password, existing email. ID

--- router/account/auth.controller.js
+++ router/account/auth.controller.js
@@ -4,6 +4,7 @@ const { createIndexes } = require('../../models/account');
 
 exports.localRegister = async (req, res) => {
     console.log(req.body);
+
     const schema = Joi.object().keys({
         Name: Joi.string().required(),
         email: Joi.string().email().required(),
@@ -17,19 +18,21 @@ exports.localRegister = async (req, res) => {
         res.status(400).json({ error: "이메일 양식을 맞춰주시고, 비밀번호는 최소 6글자입니다."})
         console.log(result.error);
         return;
-    }else if(req.body.password === req.body.Rpassword){
+
+    }else if(req.body.password !== req.body.Rpassword){
         res.status(400).json({error: "비밀번호와 재확인 비밀번호가 동일하지않습니다."})
         res.redirect("/account/register");
     }
 
     let existing = null;
     try {
-        existing = await Account.findByEmailOrUsername(req.body);
+        existing = Account.findByEmailOrID(req.body);
     }catch(e){
-        res.status(500).json({ error : e });
+        res.status(500).json({ error : e.toString() });
         return;
     }
-    if(existing){
+
+    if(existing.email || existing.ID){
         res.status(409).json({key : existing.email === req.body.email ? 'email' : 'ID'});
         return;
     }
@@ -38,7 +41,7 @@ exports.localRegister = async (req, res) => {
     try {
         account = await Account.localRegister(req.body);
     } catch(e) {
-        res.status(500).json({ error: e.toString() });
+        res.status(500).json({ error: e.toString() , code : "회원가입코드"});
     }
 
 
