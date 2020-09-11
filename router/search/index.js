2390d6ca04ab3653f53ce785738832ee966be2db
HelenHyeon <s20079@gsm.hs.kr>
2020-09-11 10:22:08 +0900

    update: search paging

--- router/search/index.js
+++ router/search/index.js
@@ -2,6 +2,41 @@ const express = require('express');
 
 const search = express.Router();
 
-search.get('/', (_, res) => res.render('search'));
+search.get('/', (req, res) =>
+    res.render('search', {
+        posts: [{
+                category: 'TMI',
+                title: '타닥이를 안아프게 맞는 발명품5',
+                description: '아잉 기모찌해',
+                Publisher: 'ICT',
+            },
+            {
+                category: 'TMI',
+                title: '타닥이를 안아프게 맞는 발명품4',
+                description: '아잉 기모찌해',
+                Publisher: 'ICT',
+            },
+            {
+                category: 'TMI',
+                title: '타닥이를 안아프게 맞는 발명품3',
+                description: '아잉 기모찌해',
+                Publisher: 'ICT',
+            }, {
+                category: 'TMI',
+                title: '타닥이를 안아프게 맞는 발명품2',
+                description: '아잉 기모찌해',
+                Publisher: 'ICT',
+            },
+            {
+                category: 'TMI',
+                title: '타닥이를 안아프게 맞는 발명품1',
+                description: '아잉 기모찌해',
+                Publisher: 'ICT',
+            },
+        ],
+        page: parseInt(req.query.page || 1),
+        postCnt: 121
+    })
+);
 
 module.exports = search;
