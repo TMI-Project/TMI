db5b8930ed08127c734eacddd0d1102800b62741
seolin <iseolin76@gmail.com>
2020-09-12 19:44:51 +0900

    Add chat-time

--- public/js/chatting.js
+++ public/js/chatting.js
@@ -56,14 +56,28 @@ function send() {
   // 가져왔으니 데이터 빈칸으로 변경
   document.getElementById('input').value = ''
 
-  // 내가 전송할 메시지 클라이언트에게 표시
+  //챗을 지정함
   var chat = document.getElementById('chat')
+
+  //현재 시간 표시
+  let today = new Date();
+  var time = document.createElement('p')
+  var exsecn = today.toLocaleTimeString().split(':')
+  var tnode = document.createTextNode(exsecn[0]+"시 "+exsecn[1]+"분")
+
+  time.classList.add('time')
+  time.appendChild(tnode)
+  chat.appendChild(time)
+
+  // 내가 전송할 메시지 클라이언트에게 표시
   var msg = document.createElement('div')
   var node = document.createTextNode(message)
+
   msg.classList.add('me')
   msg.appendChild(node)
   chat.appendChild(msg)
 
   // 서버로 message 이벤트 전달 + 데이터와 함께
+  socket.emit('time', {type: 'time', time: time})
   socket.emit('message', {type: 'message', message: message})
 }
