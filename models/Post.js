de00bd7b5e75aa7d4941bcbdd0343f1752f1468e
sunrabbit123 <qudwls185@naver.com>
2020-09-14 15:57:51 +0900

    ADD findPostAt_id

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+const {
+    Schema
+} = mongoose;
 
 const Post = new Schema({
     category : String,
@@ -10,6 +12,12 @@ const Post = new Schema({
     atDate : Date,
     img : String
 });
+
+Post.statics.findPostAt_id = function( { _id } ){
+    return this.findOne({
+        _id
+    });
+};
 
 exports.module = mongoose.model('Post', Post);
 
