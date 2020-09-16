2b60c603017936b3ff07c056a4985f16fcc56b47
sunrabbit123 <qudwls185@naver.com>
2020-09-16 23:43:31 +0900

    add userName

--- router/chatting/index.js
+++ router/chatting/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const cookies = require('../lib/cookie');
+const Account = require('../models/account');
 
 const chatting = express.Router();
 
-chatting.get('/', (_, res) => res.render('chatting'));
+chatting.get('/', async(req, res) => {
+    try{
+        _id =  await cookies.findAccount_idAtCookie(req);
+        account = await Account.findBy_id(_id);
+        userName = account.Name;
+    }catch( e ) {
+        console.log(e);
+    }
+
+    res.render('chatting'/*, {userName : userName }*/);
+});
 
 module.exports = chatting;
