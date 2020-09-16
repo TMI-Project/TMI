497a204b398f500c501612f058a638f2d3a82dd1
sunrabbit123 <qudwls185@naver.com>
2020-09-16 14:38:41 +0900

    add error catch

--- router/index.js
+++ router/index.js
@@ -10,10 +10,13 @@ router.use('/chatting', require('./chatting'));
 router.use('/calendar', require('./calendar'));
 
 router.get('/',async (req, res) => {
-
-    _id =  await cookies.findAccount_idAtCookie(req);
-    account = await Account.findBy_id(_id);
-    userName = account.Name;
+    try{
+        _id =  await cookies.findAccount_idAtCookie(req);
+        account = await Account.findBy_id(_id);
+        userName = account.Name;
+    }catch( e ) {
+        console.log(e);
+    }
 
     res.render('index' /* , {name : 'userName'} */ );
 });
